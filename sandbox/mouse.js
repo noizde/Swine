d04a5ff9919fccd51559867a286f13ed6973ed43
Enzo Dayrit <enzodayrit11@gmail.com>
2014-01-19 01:44:41 +0900

    floating menu button w/ mouseover fx

--- sandbox/mouse.js
+++ sandbox/mouse.js
@@ -1,8 +1,24 @@
 //bigger
 function effectMouseover(event) {
     var t = event.target;
-    var newX = t.scaleX + 1;
-    var newY = t.scaleY + 1;
+    var newX = 1.02;
+    var newY = 1.02;
+    t.setTransform(t.x, t.y, newX, newY, 0, 0, 0, t.regX, t.regY);
+    stage.update();
+}
+
+function effectMousedown(event) {
+    var t = event.target;
+    var newX = 0.98;
+    var newY = 0.98;
+    t.setTransform(t.x, t.y, newX, newY, 0, 0, 0, t.regX, t.regY);
+    stage.update();
+}
+
+function effectMouseout(event) {
+    var t = event.target;
+    var newX = 1;
+    var newY = 1;
     t.setTransform(t.x, t.y, newX, newY, 0, 0, 0, t.regX, t.regY);
     stage.update();
 }
